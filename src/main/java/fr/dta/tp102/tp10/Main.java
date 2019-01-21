76e05531c96d7c91418040d96d3885f315464169
Baptiste <smaug.exe@gmail.com>
2019-01-21 14:30:58 +0100

    zdzddz

--- src/main/java/fr/dta/tp102/tp10/Main.java
+++ src/main/java/fr/dta/tp102/tp10/Main.java
@@ -1,38 +1,23 @@
 package fr.dta.tp102.tp10;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
 public class Main {
 	
+	private static final  Logger logger = LoggerFactory.getLogger(Main.class);
+	
 	public static void main(String[] args) {
-		
-		/*Point p1 = new Point(3,5);
-		Rond r = new Rond(p1,2);
-		r.affiche();
-		Rectangle rect = new Rectangle(p1,2,3);
-		rect.affiche();*/
+
 
 			Point p3 = new Point(3,5);
 			Point p2 = new Point(3,5);
 			Point p4 = new Point(4,8);
 			
-			System.out.println(p3.equals(p2));
-			System.out.println(p3.equals(p4));
+			logger.info("",p3.equals(p2));
+			logger.info("",p3.equals(p4));
 
-		
-		
-		
-	/*	recursivite("Bonjour");
-	 * 
-	 * 
-	  	public static void recursivite(String s) {
-			System.out.println("le mot est : "+s);
-			if(s.length()==1) {
-				System.out.println("le mot est : ");
-				}
-				else {
-					recursivite(s.substring(0, s.length()-1));
-				}
-			} 
-			                METHODE RECURSIVE  */
 	
 	}
 
